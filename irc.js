530ef6f0d75f50b300481a76fdfd90b6ec83d652
Martin Hradil <mhradil@redhat.com>
2015-11-18 18:23:22 +0000

    irc can connect and logs stuff

--- irc.js
+++ irc.js
@@ -1,24 +1,45 @@
-var conf = require('./config.local.js');
+var r = require('rethinkdb');
 var _ = require('lodash');
 var irc_client = require('irc').Client;
 
+var servers = [];
 var clients = [];
+var connection = null;
 
-conf.servers.forEach(function(srv) {
-	var nicks = _.isArray(srv.nick) ? srv.nick : [ srv.nick ];
-	nicks.forEach(function(nick) {
-		var client = irc_client(srv.url, nick, {
-			port: srv.port || 6667,
-			channels: srv.channels,
-			autoConnect: false,
-		});
-		console.log(client);
-		clients.push(client);
+r.connect({ db: 'hirc' })
+.then(function(con) {
+	connection = con;
+})
+.then(null, function(err) {
+	console.error(err);
+	// TODO exit
+})
+.then(function() {
+	return r.table('servers').run(connection)
+	.then(function(cursor) {
+		return cursor.toArray();
+	})
+	.then(function(ary) {
+		servers = ary;
 	});
-});
+})
+.then(function() {
+	servers.forEach(function(srv) {
+		var nicks = _.isArray(srv.nick) ? srv.nick : [ srv.nick ];
+		nicks.forEach(function(nick) {
+			var client = new irc_client(srv.url, nick, {
+				port: srv.port || 6667,
+				channels: srv.channels,
+			});
 
-clients.forEach(function(client) {
-	client.addListener('message', (a,b,c) => console.log(a,b,c));
-	client.addListener('pm', (a,b,c) => console.warn(a,b,c));
-	client.addListener('error', (a,b,c) => console.error(a,b,c));
+			clients.push(client);
+		});
+	});
+})
+.then(function() {
+	clients.forEach(function(client) {
+		client.addListener('message', (a,b,c) => console.log('m', a,b,c));
+		client.addListener('pm', (a,b,c) => console.warn('p', a,b,c));
+		client.addListener('error', (a,b,c) => console.error('e', a,b,c));
+	});
 });
